e79ea377adedca2f1f24af64bbc1acaebe3cffb8
Suryavanshiaditya1999 <137984887+Suryavanshiaditya1999@users.noreply.github.com>
2024-07-13 05:13:13 +0530

    Update attendance.groovy

--- vars/attendance.groovy
+++ vars/attendance.groovy
@@ -1,1 +1,10 @@
+import org.python.CoveragePython
+import org.python.Dependency
 
+def callCoveragePython() {
+    new CoveragePython().call()
+}
+
+def callDependency() {
+    new Dependency().call()
+}
