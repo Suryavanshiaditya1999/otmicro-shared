1820af1f8ef1f7cc9910c33e3fba0016b561023f
Suryavanshiaditya1999 <137984887+Suryavanshiaditya1999@users.noreply.github.com>
2024-07-13 05:11:02 +0530

    Create SonarQube.groovy

---
+++ src/org/python/SonarQube.groovy
@@ -0,0 +1,10 @@
+def call(String projectKey, String sourcesDir, String sonarToken) {
+    def scannerHome = tool name: 'SonarQube-Scanner', type: 'hudson.plugins.sonar.SonarRunnerInstallation'
+    withSonarQubeEnv('SonarQube') {
+        sh "${scannerHome}/bin/sonar-scanner " +
+           "-Dsonar.projectKey=${projectKey} " +
+           "-Dsonar.sources=${sourcesDir} " +
+           "-Dsonar.host.url=http://localhost:9000 " +
+           "-Dsonar.login=${sonarToken}"
+    }
+}
