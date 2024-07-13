1c4119e76c000caadf99c8e044eb5617653d5ebf
Suryavanshiaditya1999 <137984887+Suryavanshiaditya1999@users.noreply.github.com>
2024-07-13 18:27:17 +0530

    Update attendance.groovy

--- vars/attendance.groovy
+++ vars/attendance.groovy
@@ -1,20 +1,21 @@
-import org.python.CoveragePython
-import org.python.Dependency
-import org.python.SonarQube
-import org.python.UnitTesting
+// import org.python.CoveragePython
+// import org.python.Dependency
+// import org.python.SonarQube
+// import org.python.UnitTesting
+import org.python.Checkout
 
 def call(String gitUrl, String branch = 'main') {
     new Checkout().call(gitUrl, branch)
 }
 
-def callDependency() {
-    new Dependency().call()
-}
+// def callDependency() {
+//     new Dependency().call()
+// }
 
-def callSonarqube() {
-    new UnitTesting().call()
-}
+// def callSonarqube() {
+//     new UnitTesting().call()
+// }
 
-def callUnitTesting() {
-    new UnitTesting().call()
-}
+// def callUnitTesting() {
+//     new UnitTesting().call()
+// }
